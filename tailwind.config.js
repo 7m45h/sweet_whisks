9c62adc4a8bc3bc6d7fab03ef729ca92b7c3bbef
TSach2003 <tsaar788@gmail.com>
2024-09-27 11:14:05 +0530

    About us section finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,19 +6,19 @@ module.exports = {
         fontSize: {
           //Text styles desktop   
           'h1-desktop': ['3.81rem', {lineHeight: '1.1', fontWeight: 700}],
-          'h2-desktop': ['3.06rem', {lineHeight: '1.1'}],
+          'h2-desktop': ['3.06rem', {lineHeight: '1.1', fontWeight: 600}],
           'a-desktop': ['1.25rem', {lineHeight: '1.4'}],
           'p-desktop': ['1rem', {lineHeight: '1.5'}],
           'p-small-desktop': ['0.875rem', {lineHeight: '1.6'}],
           //Text styles tablet
           'h1-tablet': ['3rem', {lineHeight: '1.05', fontWeight: 700}],
-          'h2-tablet': ['2.5rem', {lineHeight: '1.05'}],
+          'h2-tablet': ['2.5rem', {lineHeight: '1.05', fontWeight: 600}],
           'a-tablet': ['1.19rem', {lineHeight: '1.35'}],
           'p-tablet': ['1rem', {lineHeight: '1.45'}],
           'p-small-tablet': ['0.875rem', {lineHeight: '1.55'}],
           //Text styles mobile
           'h1-mobile': ['2rem', {lineHeight: '1', fontWeight: 700}],
-          'h2-mobile': ['1.812rem', {lineHeight: '1'}],
+          'h2-mobile': ['1.812rem', {lineHeight: '1', fontWeight: 600}],
           'a-mobile': ['1.125rem', {lineHeight: '1.3'}],
           'p-mobile': ['1rem', {lineHeight: '1.4'}],
           'p-small-mobile': ['0.875rem', {lineHeight: '1.5'}],
@@ -31,6 +31,7 @@ module.exports = {
           'primary': '#5FCAC7',
           'accent': '#4CA29F',
           'secondary': '#FD78AD',
+          'secondary-shade': '#CA608A',
           'white': '#FFFFFF',
           'background': '#F0FAFA',
           'txt-darkGray': '#3A4343',
